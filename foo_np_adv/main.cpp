b865b6101a34435f53ab0469f710d9861e8e8bf4
siska <io@nixnodes.net>
2018-09-21 22:02:35 +0200

    Change the stupid description

--- foo_np_adv/main.cpp
+++ foo_np_adv/main.cpp
@@ -1,4 +1,4 @@
 #include "stdafx.h"
 
-DECLARE_COMPONENT_VERSION(COMMON_NAME,"0.8.1","Writes different formatted strings to different files.");
+DECLARE_COMPONENT_VERSION(COMMON_NAME,"0.8.1","Writes formatted strings to files.");
 VALIDATE_COMPONENT_FILENAME("foo_np_advanced.dll");
