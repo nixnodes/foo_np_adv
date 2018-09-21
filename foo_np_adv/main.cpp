1fcb4be2982da2ab947123be58eba168f2633720
siska <io@nixnodes.net>
2018-09-21 21:59:36 +0200

    Bump to 0.8.1

--- foo_np_adv/main.cpp
+++ foo_np_adv/main.cpp
@@ -1,4 +1,4 @@
 #include "stdafx.h"
 
-DECLARE_COMPONENT_VERSION(COMMON_NAME,"0.8","Writes different formatted strings to different files.");
+DECLARE_COMPONENT_VERSION(COMMON_NAME,"0.8.1","Writes different formatted strings to different files.");
 VALIDATE_COMPONENT_FILENAME("foo_np_advanced.dll");
