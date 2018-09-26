af10a0f07eece3da4d8d926c030248f9fd4dc223
siska <io@nixnodes.net>
2018-09-26 20:08:57 +0200

    Bump to 0.9.6

--- foo_np_adv/main.cpp
+++ foo_np_adv/main.cpp
@@ -1,4 +1,4 @@
 #include "stdafx.h"
 
-DECLARE_COMPONENT_VERSION(COMMON_NAME, "0.9.5", "Writes formatted strings to files.");
+DECLARE_COMPONENT_VERSION(COMMON_NAME, "0.9.6", "Writes formatted strings to files.");
 VALIDATE_COMPONENT_FILENAME("foo_np_advanced.dll");
