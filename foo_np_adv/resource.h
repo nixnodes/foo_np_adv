66dda5704c6fdefcc91dfc0b46804aca3391e32c
siska <io@nixnodes.net>
2018-09-29 00:20:20 +0200

    Cleanup

--- foo_np_adv/resource.h
+++ foo_np_adv/resource.h
@@ -3,36 +3,28 @@
 // Used by foo_npa.rc
 //
 #define IDD_NPAPREFERENCES              148
-#define IDC_BOGO1                       1001
-#define IDC_BOGO2                       1002
-#define IDC_LIST1                       1004
-#define IDC_COMBO1                      1005
-#define IDC_ENCODING                    1006
-#define IDC_EDIT1                       1007
-#define IDC_FILENAME                    1007
-#define IDC_CHECK1                      1008
-#define IDC_BUTTON1                     1009
-#define IDC_BUTTON2                     1010
-#define IDC_CHECK2                      1011
-#define IDC_SLIDER1                     1012
-#define IDC_PATTERN                     1012
-#define IDC_PREVIEW                     1013
-#define IDC_CONTEXTMENU_EVENTS          1014
-#define IDC_BUTTON3                     1015
-#define IDC_CHECK3                      1016
-#define IDC_CHECK5                      1017
-#define IDC_DELAY                       1018
-#define IDC_SPIN2                       1019
-#define IDC_SPIN1                       1019
-#define IDC_ON_EXIT                     1020
-#define IDC_CHECK4                      1021
-#define IDC_STATIC5                     1022
-#define IDC_CHECK6                      1023
-#define IDC_BUTTON4                     1024
-#define IDC_CHECK7                      1025
-#define IDC_ENABLED                     1025
-#define IDC_BUTTON6                     1026
-#define IDC_CONTEXT_CONFIG              1026
+#define IDC_COMBO1                      1001
+#define IDC_ENCODING                    1002
+#define IDC_FILENAME                    1003
+#define IDC_CHECK1                      1004
+#define IDC_BUTTON1                     1005
+#define IDC_BUTTON2                     1006
+#define IDC_CHECK2                      1007
+#define IDC_PATTERN                     1008
+#define IDC_PREVIEW                     1009
+#define IDC_CONTEXTMENU_EVENTS          1010
+#define IDC_BUTTON3                     1011
+#define IDC_CHECK3                      1012
+#define IDC_CHECK5                      1013
+#define IDC_DELAY                       1014
+#define IDC_SPIN1                       1015
+#define IDC_ON_EXIT                     1016
+#define IDC_CHECK4                      1017
+#define IDC_STATIC5                     1018
+#define IDC_CHECK6                      1019
+#define IDC_BUTTON4                     1020
+#define IDC_ENABLED                     1021
+#define IDC_CONTEXT_CONFIG              1022
 
 // Next default values for new objects
 // 
